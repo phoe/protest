02c791ae2b6177cd24f9dff828e456aa309b0c9f
Michal Herda <michal.herda@ericsson.com>
2018-09-25 16:38:37 +0200

    add :TYPE to addressable

--- src/common/mixin/addressed.lisp
+++ src/common/mixin/addressed.lisp
@@ -15,20 +15,25 @@ exported as a human-readable string.
 \
 A default method is provided on the ADDRESS function that concatenates the ~
 hostname and the port with a #\: character in between. Classes are free to ~
-implement this method in subclasses."
+implement this method in subclasses.
+\
+All generic functions specified in this protocol specify an optional TYPE ~
+argument. This argument may be used to differentiate between different types ~
+of network addresses. Methods specified on these generic functions may specify ~
+a default type on the keyword."
      :tags (:addressed)
      :export t)
   (:class addressed () ())
   "An addressed object. See protocol ADDRESSED for details."
-  (:function hostname ((addressed addressed)) string)
+  (:function hostname ((addressed addressed) &optional type) string)
   "Returns the addressed object's hostname."
-  (:function port ((addressed addressed)) unsigned-byte)
+  (:function port ((addressed addressed) &optional type) unsigned-byte)
   "Returns the addressed object's port."
-  (:function address ((addressed addressed)) string)
+  (:function address ((addressed addressed) &optional type) string)
   "Returns the address of the addressed object socket in a string form for ~
 textual representation.")
 
 (execute-protocol addressed)
 
-(defmethod address ((addressed addressed))
-  (format nil "~A:~A" (hostname addressed) (port addressed)))
+(defmethod address ((addressed addressed) &optional type)
+  (format nil "~A:~A" (hostname addressed type) (port addressed type)))
