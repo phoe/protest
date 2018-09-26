019fb180cad8ee51821a5ca5a28710c7e22539ad
Michal Herda <michal.herda@ericsson.com>
2018-09-26 15:53:59 +0200

    Expand HANDLING protocol

--- src/common/mixin/handling.lisp
+++ src/common/mixin/handling.lisp
@@ -12,6 +12,11 @@
     (:documentation "The HANDLING protocol describes objects which have a
 handler function.
 \
+All generic functions specified in this protocol specify an optional TYPE ~
+argument. This argument may be used to differentiate between different types ~
+of network addresses. Methods specified on these generic functions may specify ~
+a default type on the keyword.
+\
 This protocol specifies only a mechanism of accessing the handler function and
 does not concern itself with the arity or arguments of the handler function
 itself."
@@ -19,9 +24,9 @@ itself."
      :export t)
   (:class handling () ())
   "An object with a handler. See protocol HANDLING for details."
-  (:function handler ((object handling)) function)
+  (:function handler ((object handling) &optional type) function)
   "Returns the handler function of the object."
-  (:function (setf handler) (new-value (object handling)) t)
+  (:function (setf handler) (new-value (object handling) &optional type) t)
   "Sets the handler function of the object.")
 
 (execute-protocol handling)
